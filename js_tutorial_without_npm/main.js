44cbfe671897ed210a04ca38a6946031eb7c5fb1
Simon Gebreselassie <simonabra35@gmail.com>
2020-10-31 05:25:20 -0400

    gameset

---
+++ js_tutorial_without_npm/main.js
@@ -0,0 +1,21 @@
+
+/*alert(new Phrase ("Madam, I'm Adam.").palindrome());*/
+
+
+function palindromeTester(){
+let string = prompt("Pleaseee enter= a string for palindrome testing");
+let phrase = new Phrase(string);
+if(phrase.palindrome()){
+  alert(`"${phrase.content}" is a palindrome`)
+}
+else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
+
+}
+document.addEventListener("DOMContentLoaded",function() {
+let button = document.querySelector("#palindromeTester");
+button.addEventListener("click",function(){
+  palindromeTester();
+});
+});
